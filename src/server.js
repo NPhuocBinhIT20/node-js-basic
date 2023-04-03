ac39ec3ab3b979b8a2cd621ea2a11285fe6429b2
unknown <binhnguyen110120@gmail.com>
2023-04-03 17:22:41 +0700

    03/04/2023 Nodejs v2

--- src/server.js
+++ src/server.js
@@ -1,18 +1,14 @@
 // const express = require("express");
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
+import initWebRoute from "./routes/web";
 require("dotenv").config();
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
 
 configViewEngine(app);
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+initWebRoute(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
